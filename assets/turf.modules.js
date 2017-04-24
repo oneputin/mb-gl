75b99b0116c491b028a0a78ac4a541cbb6162099
oneputin <wgpagenkopf@gmail.com>
2017-04-24 07:53:49 +0200

    try fullscreen map

---
+++ assets/turf.modules.js
@@ -0,0 +1,11 @@
+// 
+turf.point(cc.slice(0,2))
+turf.bbox(turf.featureCollection(ffa))
+turf.featureCollection(ffa)
+turf.polygon([[]])
+turf.inside(p0, poly)
+turf.bearing()
+turf.lineDistance(legToArrival, "kilometers")
+turf.along(leg, dist, 'kilometers')
+turf.pointOnLine(indexedFeature, queryPnt, 'kilometers')
+
